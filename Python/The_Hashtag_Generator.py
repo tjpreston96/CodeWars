92139b1712132e3093165607ab20c27d132ba273
Tyler Preston <tpreston96@gmail.com>
2022-08-14 14:04:23 -0500

    The Hashtag Generator

---
+++ Python/The_Hashtag_Generator.py
@@ -0,0 +1,17 @@
+# Prompt: https://www.codewars.com/kata/52449b062fb80683ec000024/python
+
+# Expected Result:
+# " Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+
+# Answer:
+def generate_hashtag(s):
+    return f"#{s.title().replace(' ','')}" if 0 < len(s.strip()) <= 140 else False
+
+
+# def generate_hashtag(s):
+#     result = f'#{s.title().replace(" ", "")}'
+
+#     if s.strip() == "" or len(result) > 140:
+#         return False
+#     else:
+#         return result
